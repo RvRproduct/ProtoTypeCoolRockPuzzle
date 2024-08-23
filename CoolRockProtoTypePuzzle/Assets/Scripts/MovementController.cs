6daac53cfbc2055d448e2de4fe361f5d45cbd156
Roberto Valentino Reynoso <68643525+RvRproduct@users.noreply.github.com>
2024-08-23 13:45:52 -0600

    Character Controller

---
+++ CoolRockProtoTypePuzzle/Assets/Scripts/MovementController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementController : MonoBehaviour
+{
+    PlayerControl playerControl;
+    CharacterController characterController;
+
+    Vector2 currentMovementInput;
+    Vector3 currentMovement;
+    bool isMovementPressed;
+    float rotationFactorPerFrame = 15.0f;
+
+    private void Awake()
+    {
+        playerControl = new PlayerControl();
+        characterController = GetComponent<CharacterController>();
+
+        playerControl.CharacterControl.Move.started += onMovementInput;
+        playerControl.CharacterControl.Move.canceled += onMovementInput;
+        playerControl.CharacterControl.Move.performed += onMovementInput;
+    }
+
+    void handleRotation()
+    {
+        Vector3 positionToLookAt;
+
+        positionToLookAt.x = currentMovement.x;
+        positionToLookAt.y = 0.0f;
+        positionToLookAt.z = currentMovement.z;
+
+        Quaternion currentRotation = transform.rotation;
+
+        if (isMovementPressed)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame * Time.deltaTime);
+        }
+        
+    }
+
+    void onMovementInput (InputAction.CallbackContext context)
+    {
+        currentMovementInput = context.ReadValue<Vector2>();
+        currentMovement.x = currentMovementInput.x;
+        currentMovement.z = currentMovementInput.y;
+        isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+    }
+
+    void handleGravity()
+    {
+        if (characterController.isGrounded)
+        {
+            float groundedGravity = -0.05f;
+            currentMovement.y = groundedGravity;
+        }
+        else
+        {
+            float gravity = -9.8f;
+            currentMovement.y += gravity;
+        }
+    }
+
+    void Update()
+    {
+        handleRotation();
+        characterController.Move(currentMovement * Time.deltaTime);
+    }
+
+    private void OnEnable()
+    {
+        playerControl.CharacterControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControl.CharacterControl.Disable();
+    }
+}
