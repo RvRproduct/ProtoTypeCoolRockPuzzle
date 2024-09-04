b40724d2f0b877ff2afd2ccd56b5c48934627f7f
amazingunicornn <alicewang6538@gmail.com>
2024-09-04 10:03:14 -0600

    ADD raycast for debugging

--- CoolRockProtoTypePuzzle/Assets/Scripts/Player/MovementController.cs
+++ CoolRockProtoTypePuzzle/Assets/Scripts/Player/MovementController.cs
@@ -92,6 +92,26 @@ public class MovementController : MonoBehaviour
         }
         
     }
+    void FixedUpdate()
+    {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+        
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if(isAiming)
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 50, Color.red);
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 50, layerMask))
+            {
+                Debug.Log("Hit something");
+            }
+        }
+    }
     private void OnDisable()
     {
         PlayerInputManager.OnPlayerMovement -= onMovementInput;
